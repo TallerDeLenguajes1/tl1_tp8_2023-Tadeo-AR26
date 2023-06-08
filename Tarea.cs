2dd32d43b704a97e730823a006aaada874058973
Tadeo-AR26 <alonsoruiztadeo26@gmail.com>
2023-06-08 17:45:15 -0300

    clase Tareas

---
+++ Tarea.cs
@@ -0,0 +1,13 @@
+namespace Espacio.Tareas{
+    public class Tareas{
+        private int tareaID;
+        private string? descripcion;
+        private int duracion;
+
+        public int TareaID { set => tareaID = value; }
+        public string Descripcion { set => descripcion = value; }
+        public int Duracion { set => duracion = value; }
+
+        
+    }
+}
